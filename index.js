1ab9dd513f809d180371bca5b850db0eb3e89623
Clément <clement@anis.local>
2021-11-23 14:23:21 +0100

    merge

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express')
-const fastify = require('fastify')({ logger: true })
+const http = require('http');
+let app = express();
+
+app.get('/about', function (req, res) {
+    res.send('GET request to the about page');
+});
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("Hello world")
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
