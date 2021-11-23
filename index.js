daa6e7397247550a74c5efb3784c3a02df8ef5c3
Clément <clement@anis.local>
2021-11-23 16:34:34 +0100

    connect to mongodb

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express')
-const http = require('http');
 let app = express();
 
-app.get('/about', function (req, res) {
-    res.send('GET request to the about page');
+app.get('/', function (req, res) {
+    res.send(' Hello World');
 });
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end("Hello world")
+app.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://groupe1:yaSMiFaHeHTDfy65TKA@94.130.108.19/groupe1', {useNewUrlParser: true, useUnifiedTopology: true});
+db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("Connected to Mongoose")
 });
