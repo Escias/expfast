52ae8aeffa043364f3c16ccf17db3bf796780c58
Jimmy <ljimmy98@hotmail.fr>
2021-11-23 14:22:17 +0100

    remove require http

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ app.get('/', function (req, res) {
     res.send('GET request to the about page');
 });
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
