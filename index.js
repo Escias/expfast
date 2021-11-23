44431dfec85a9870d993dfe331a8071ca4a660f8
Jimmy <ljimmy98@hotmail.fr>
2021-11-23 14:21:39 +0100

    remove require http

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const express = require('express')
-const http = require('http');
 let app = express();
 
-app.get('/about', function (req, res) {
+app.get('/', function (req, res) {
     res.send('GET request to the about page');
 });
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!\n');
-});
-
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
