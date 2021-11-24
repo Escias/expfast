716b94b23fbce57319ff9da564ae4f9a5711a14a
Jimmy <ljimmy98@hotmail.fr>
2021-11-24 10:37:37 +0100

    add nunjucks / replace sendFile by render

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config({path: '.env'})
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require("mongoose")
+const nunjucks = require('nunjucks')
+
 let app = express()
 
 const hostname = '127.0.0.1';
@@ -10,6 +12,27 @@ const port = 3000;
 let corsOptions = {
     origin: "http://localhost:3000"
 };
+
+app.use(cors(corsOptions))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+
+app.get('/', function (req, res) {
+    res.render('index.html')
+});
+
+app.get('/login', function (req, res) {
+    res.render('login.html')
+});
+
+app.get('/create', function (req, res) {
+    res.render('create/html')
+});
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
@@ -19,25 +42,5 @@ mongoose.connect('mongodb://groupe1:yaSMiFaHeHTDfy65TKA@94.130.108.19/groupe1', 
 db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-  console.log("Connected to Mongoose")
-
-
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.get('/', function (req, res) {
-    res.sendFile('views/index.html', {root: __dirname});
-});
-
-app.get('/login', function (req, res) {
-    res.sendFile('views/login.html', {root: __dirname});
-});
-
-app.get('/create', function (req, res) {
-    res.sendFile('views/create.html', {root: __dirname});
-});
-
-app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log("Connected to Mongoose")
 });
